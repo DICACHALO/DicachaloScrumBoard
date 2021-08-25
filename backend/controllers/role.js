48a55247eea05be16ba52d251df67b50d65a86f8
Diana Chacon <dchacon871@unab.edu.co>
2021-08-24 19:21:52 -0500

    update role

--- backend/controllers/role.js
+++ backend/controllers/role.js
@@ -23,7 +23,7 @@ return res.status(200).send({role});
 
 // En el req llega una api pero sin ningún JSON porque no se necesita
 const listRole = async (req, res) => {
-    const role = await Role.find()
+    let role = await Role.find()
     if(!role || role.length === 0 ) return res.status(401).send("No role");
     return res.status(200).send({role});
 };
